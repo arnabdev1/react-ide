1b42890260a9e453dfed4123a8aa601a90655484
arnabdev-bs <arnab.bondstein@gmail.com>
2024-08-01 09:40:08 +0600

    minor-changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Center, Text } from "@chakra-ui/react";
 import CodeEditor from "./components/CodeEditor";
 
 function App() {
   return (
-    <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
+    <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={2}>
+      <Text fontSize="4xl" color="#00ff00" fontWeight="bolder" mb={0} textAlign="center">
+        Arnab's IDE
+      </Text>
       <CodeEditor />
     </Box>
   );
